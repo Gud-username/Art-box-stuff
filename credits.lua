52d69c1101a0cbec8287f4b75563b00ed54eafad
Lusty joker <toby.hayter10@gmail.com>
2025-08-06 12:25:45 +0100

    Update credits.lua

--- credits.lua
+++ credits.lua
@@ -19,7 +19,7 @@ SMODS.current_mod.extra_tabs = function()
                     n = G.UIT.R,
                     config = { align = 'cm', minh = 0.25 },
                     nodes = {
-                      { n = G.UIT.T, config = { text = 'OppositeWolf770', colour = G.C.PURPLE, scale = 0.4 } }
+                      { n = G.UIT.T, config = { text = "Smol (kinda)", colour = G.C.PURPLE, scale = 0.4 } }
                     }
                   },
                 }
@@ -38,21 +38,21 @@ SMODS.current_mod.extra_tabs = function()
                 n = G.UIT.R,
                 config = { align = 'cm', minh = 0.25 },
                 nodes = {
-                  { n = G.UIT.T, config = { text = "", colour = G.C.GREEN, scale = 0.4 } }
+                  { n = G.UIT.T, config = { text = "SadCube", colour = G.C.GREEN, scale = 0.4 } }
                 }
               },
               {
                 n = G.UIT.R,
                 config = { align = 'cm', minh = 0.25 },
                 nodes = {
-                  { n = G.UIT.T, config = { text = "", colour = G.C.GREEN, scale = 0.4 } }
+                  { n = G.UIT.T, config = { text = "Samuran", colour = G.C.GREEN, scale = 0.4 } }
                 }
               },
               {
                 n = G.UIT.R,
                 config = { align = 'cm', minh = 0.25 },
                 nodes = {
-                  { n = G.UIT.T, config = { text = "", colour = G.C.GREEN, scale = 0.4 } }
+                  { n = G.UIT.T, config = { text = "Superb", colour = G.C.GREEN, scale = 0.4 } }
                 }
               },
             }
