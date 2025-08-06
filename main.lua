df5e8c0b9f6b40f070dbd73391778061aeba7ceb
Lusty joker <toby.hayter10@gmail.com>
2025-08-06 12:24:46 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
+-- Load the vanilla Joker textures
+SMODS.load_file('content/Jokers.lua')()
+
 -- Load the vanilla tarot textures
 SMODS.load_file('content/Tarots.lua')()
-
--- Load the vanilla voucher textures
-SMODS.load_file('content/Vouchers.lua')()
 
 -- Load the vanilla planet textures
 SMODS.load_file('content/Planets.lua')()
@@ -10,8 +10,8 @@ SMODS.load_file('content/Planets.lua')()
 -- Load the vanilla Spectral textures
 SMODS.load_file('content/Spectrals.lua')()
 
--- Load the vanilla Joker textures
-SMODS.load_file('content/Jokers.lua')()
+-- Load the vanilla voucher textures
+SMODS.load_file('content/Vouchers.lua')()
 
 TexturePack {
     key = "artboxjokers",
